ac99127cb7abeafce07bdd6dacf4f20879d12e49
assem22 <assem.mukhamadi@mail.ru>
2020-09-30 10:18:39 +0600

    new

--- src/app/all/categories/breed/breed.component.ts
+++ src/app/all/categories/breed/breed.component.ts
@@ -1,12 +1,13 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, OnChanges, AfterContentChecked} from '@angular/core';
 import {Breed} from '../../model/breed';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-breed',
   templateUrl: './breed.component.html',
   styleUrls: ['./breed.component.css']
 })
-export class BreedComponent implements OnInit {
+export class BreedComponent implements OnInit, OnChanges, AfterContentChecked {
   @Input() searchText: string;
   listOfBreed: Breed[] = [
     {id: 1, name: 'Breed of pet 1', image: 'https://mdbootstrap.com/img/Photos/Others/img3.jpg'},
@@ -17,9 +18,21 @@ export class BreedComponent implements OnInit {
     {id: 6, name: 'Breed of pet 6', image: 'https://mdbootstrap.com/img/Photos/Others/img8.jpg'}
   ];
 
+  showChild = false;
+
   constructor() { }
+
+  // tslint:disable-next-line:typedef
+  ngOnChanges() { console.log('ParentComponent:OnChanges'); }
+
+  // tslint:disable-next-line:typedef
+  ngAfterContentChecked() { console.log('ParentComponent:AfterContentChecked'); }
 
   ngOnInit(): void {
   }
 
+  // tslint:disable-next-line:typedef
+  showCertainBreeds(){
+    this.showChild = !this.showChild;
+  }
 }
--- src/app/all/categories/certain-breed/certain-breed.component.ts
+++ src/app/all/categories/certain-breed/certain-breed.component.ts
@@ -1,15 +1,47 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  OnDestroy,
+  Output, AfterViewChecked
+} from '@angular/core';
 
 @Component({
   selector: 'app-certain-breed',
   templateUrl: './certain-breed.component.html',
   styleUrls: ['./certain-breed.component.css']
 })
-export class CertainBreedComponent implements OnInit {
+export class CertainBreedComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, OnDestroy, AfterViewChecked{
   @Input() searchText: string;
+  @Output() showChild: EventEmitter<boolean> = new EventEmitter();
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  // tslint:disable-next-line:typedef
+  ngOnChanges() { console.log('ChildComponent:OnChanges'); }
+  // tslint:disable-next-line:typedef
+  ngOnInit() { console.log('ChildComponent:OnInit'); }
+  // tslint:disable-next-line:typedef
+  ngDoCheck() { console.log('ChildComponent:DoCheck'); }
+  // tslint:disable-next-line:typedef
+  ngAfterContentInit() { console.log('ChildComponent:AfterContentInit'); }
+  // tslint:disable-next-line:typedef
+  ngAfterContentChecked(){console.log('ChildComponent:AfterContentChecked'); }
+  // tslint:disable-next-line:typedef
+  ngAfterViewInit() { console.log('ChildComponent:AfterViewInit'); }
+  // tslint:disable-next-line:typedef
+  ngAfterViewChecked() { console.log('ChildComponent:AfterViewChecked'); }
+  // tslint:disable-next-line:typedef
+  ngOnDestroy() { console.log('ChildComponent:OnDestroy'); }
 
+
+  // tslint:disable-next-line:typedef
+  hideChild(){
+    this.showChild.emit();
+  }
 }
