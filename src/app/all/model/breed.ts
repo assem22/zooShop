253d9c01da9402f35aed83d94d0358f0ba41a197
assem22 <assem.mukhamadi@mail.ru>
2020-12-08 21:25:52 +0600

    final

--- src/app/all/model/breed.ts
+++ src/app/all/model/breed.ts
@@ -5,17 +5,22 @@ export class Breed{
   category: string;
   name: string;
   image: string;
+  image2: string;
+  image3: string;
   information: string;
   cost: number;
 
 
-  constructor(id: number, breedId: number, category: string, name: string, image: string, information: string, cost: number) {
+  constructor(id: number, breedId: number, category: string, name: string,
+              image: string, image2: string, image3: string, information: string, cost: number) {
     this.id = id;
     this.breedId = breedId;
     this.likeStatus = false;
     this.category = category;
     this.name = name;
     this.image = image;
+    this.image2 = image2;
+    this.image3 = image3;
     this.information = information;
     this.cost = cost;
   }
